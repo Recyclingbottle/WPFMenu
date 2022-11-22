0e7ba2cb87d796a20b628963f60dddf223e6609f
Recyclingbottle <hoholeo382@gmail.com>
2022-11-22 18:33:27 +0900

    초기 커밋.

---
+++ WPFMenu/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRed(object sender, RoutedEventArgs e)
+        {
+            BackPanel.Background = Brushes.Red;
+        }
+
+        private void OnGreen(object sender, RoutedEventArgs e)
+        {
+            BackPanel.Background = Brushes.Green;
+        }
+
+        private void OnBlue(object sender, RoutedEventArgs e)
+        {
+            BackPanel.Background = Brushes.Blue;
+        }
+
+        private void OnWhite(object sender, RoutedEventArgs e)
+        {
+            BackPanel.Background = Brushes.White;
+        }
+    }
+}
