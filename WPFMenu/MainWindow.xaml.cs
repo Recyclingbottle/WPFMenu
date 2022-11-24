407fc5816265dc35227d7a3e43bfb6b72350d127
Recyclingbottle <hoholeo382@gmail.com>
2022-11-24 12:39:16 +0900

    파일 열기 버튼 추가

--- WPFMenu/MainWindow.xaml.cs
+++ WPFMenu/MainWindow.xaml.cs
@@ -23,26 +23,73 @@ namespace WPFMenu
         public MainWindow()
         {
             InitializeComponent();
+            CommandBinding bind = new CommandBinding(ApplicationCommands.Open);
+            bind.Executed += OpenDoucument;
+            CommandBindings.Add(bind);
         }
+        private void OpenDoucument(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            //dialog.FileName = "사진"; // Default file name
+            dialog.DefaultExt = ".jpg"; // Default file extension
+            dialog.Filter = "Images (.jpg)|*.jpg"; // Filter files by extension
 
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result != true)
+                return;
+
+            // Open document
+            string filename = dialog.FileName;
+
+            MessageBox.Show(filename);
+        }
         private void OnRed(object sender, RoutedEventArgs e)
         {
             BackPanel.Background = Brushes.Red;
+            ItemRed.IsChecked = true;
+            ItemGreen.IsChecked = false;
+            ItemBlue.IsChecked = false;
+
+            ItemWhite.IsEnabled = true;
         }
 
         private void OnGreen(object sender, RoutedEventArgs e)
         {
-            BackPanel.Background = Brushes.Green;
+            SolidColorBrush brush
+                //= new SolidColorBrush(Colors.Green);
+                = new SolidColorBrush(Color.FromRgb(128, 255, 128));
+            BackPanel.Background = brush;
+            ItemRed.IsChecked = false;
+            ItemGreen.IsChecked = true;
+            ItemBlue.IsChecked = false;
+
+            ItemWhite.IsEnabled = true;
         }
 
         private void OnBlue(object sender, RoutedEventArgs e)
         {
-            BackPanel.Background = Brushes.Blue;
+            SolidColorBrush brush
+                //= new SolidColorBrush(Colors.Green);
+                = new SolidColorBrush(Color.FromArgb(255, 0, 0, 255));
+            BackPanel.Background = brush;
+            ItemRed.IsChecked = false;
+            ItemGreen.IsChecked = false;
+            ItemBlue.IsChecked = true;
+
+            ItemWhite.IsEnabled = true;
         }
 
         private void OnWhite(object sender, RoutedEventArgs e)
         {
             BackPanel.Background = Brushes.White;
+            ItemRed.IsChecked = false;
+            ItemGreen.IsChecked = false;
+            ItemBlue.IsChecked = false;
+
+            ItemWhite.IsEnabled = false;
         }
     }
 }
